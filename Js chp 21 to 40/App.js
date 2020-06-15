48f72c1cd5033b2c050dd9b3c3c76616402882e0
Ali Zain <51964332+AliZaiN-157@users.noreply.github.com>
2020-06-15 15:51:34 -0700

    new assignment

---
+++ Js chp 21 to 40/App.js
@@ -0,0 +1,82 @@
+/// Chp 21 to 25 ///
+
+//          Task 1  
+// var firstname = prompt("Type your first name here")
+// var lastname = prompt("Type your Last name here") 
+
+// fullname = firstname + lastname
+
+// alert("Hello ! " + fullname)
+
+//          Task 2
+// var favmobile = prompt("Favorite Mobile Phone")
+// length = favmobile.length
+// document.write("My favorite phone is : " + favmobile + "<br>")
+// document.write("length of string : " + length + "<br>")
+
+//           Task 3
+// var x = "Pakistani"
+// y = x.indexOf("n")
+// document.write("String : " + x + "<br>")
+// document.write("index of 'n' is : " + y)
+
+//           Task 4 
+// var x = "Hello World"
+//  y = x.lastIndexOf("l")
+//  document.write("String : " + x + "<br>")
+//  document.write("Last index of 'l' is : " + y)
+
+//           Task 5
+// var x = "Pakistani"
+// y = x.slice(3,4)
+// document.write("String : " + x + "<br>")
+// document.write("Character of index 3 :" + y + "<br>")
+
+//           Task 6
+// var firstname = prompt("Type your first name here")
+// var lastname = prompt("Type your Last name here") 
+
+// fullname = firstname.concat(lastname)
+
+// alert("Hello ! " + fullname)
+
+//           Task 7
+// var City = "Hyderabad";
+// var City1 = City.replace("Hyder","Islam");
+// document.write("City : " + City + "<br>")
+// document.write("After Replacement : " + City1 + "<br>")
+
+//           Task 8
+
+//           Task 9
+// var str = "472"
+// var int = parseInt(str)
+// document.write(" String : " + str + "<br>")
+// document.write(" Str data type  : " + typeof str + "<br>")
+// document.write(" Integer : " + int + "<br>")
+// document.write(" int data type : " + typeof int + "<br>")
+
+//           Task 10
+// var input = prompt("Type here","Hello World")
+// x = input.toUpperCase();
+// document.write(input + "<br>")
+// document.write(x + "<br>")
+
+//           Task 11
+// var input = prompt("Type here","javascript")
+// y = input.toLowerCase();
+// z = y.slice(0,1)
+// x = y.slice(1)
+
+// output = z.toUpperCase()  + x + "<br>"
+// document.write(output)
+
+//           Task 12
+var num = 25.25
+x = num.toString()
+y = x.indexOf(".")
+z = x.splice(2,1,"ok")
+document.write(z)
+
+
+
